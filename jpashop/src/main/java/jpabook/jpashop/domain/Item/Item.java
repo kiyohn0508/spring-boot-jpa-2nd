dcfa8063c4156321c70dea837c7a2ab582e869de
kiyohn0508 <kiyohn0508@gmail.com>
2025-07-11 14:24:29 +0000

    feat : entity class 1

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain.Item;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="dtpye")
+@Getter @Setter
+public abstract class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+    
+    private String name;
+    private int price;
+    private int stockQuantity;
+}
