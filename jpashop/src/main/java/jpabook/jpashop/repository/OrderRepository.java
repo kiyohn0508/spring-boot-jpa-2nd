7e5ba80365ccaf49cd7bc3b23797f615f153fc1f
kiyohn0508 <kiyohn0508@gmail.com>
2025-07-16 14:09:44 +0000

    fix : order domain

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,12 +1,23 @@
 package jpabook.jpashop.repository;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Root;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Predicate;
+
+import jpabook.jpashop.domain.Member;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -22,11 +33,71 @@ public class OrderRepository {
     }
 
     // 주문 검색 기능
-    public List<Order> findAll(OrderSearch OrderSearch){
-        return em.createQuery("select o from Order o join o.member m"
-        + " where o.status =:status" + " and m.name like :name", Order.class)
-        .setParameter("status", OrderSearch.getOrderStatus())
-        .setParameter("name", OrderSearch.getMemberName())
-        .setMaxResults(100).getResultList();
+ public List<Order> findAll(OrderSearch orderSearch) {
+        //language=JPAQL
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+        if (isFirstCondition) {
+            jpql += " where";
+            isFirstCondition = false;
+        } else {
+            jpql += " and";
+        }
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+        if (isFirstCondition) {
+            jpql += " where";
+            isFirstCondition = false;
+        } else {
+            jpql += " and";
+        }
+            jpql += " m.name like :name";
+        }
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+        .setMaxResults(1000); //최대 1000건
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList(); 
     }
+
+    // JPA Creiteria
+    public List<Order> findAllCriteria(OrderSearch orderSearch){
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
+        List<Predicate> criteria = new ArrayList<>();
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"),
+            orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name =
+            cb.like(m.<String>get("name"), "%" +
+
+            orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
+        return query.getResultList();
+    }
+
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+    List<Order> orders = em.createQuery("select o from Order o where o.member.name = :name", Order.class)
+            .setParameter("name", "kim")
+            .getResultList();
+    return  orders;
+}
 }
