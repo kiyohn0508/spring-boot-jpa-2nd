f48247c27eaafac66cdafc703f3f50efd6ea5138
kiyohn0508 <kiyohn0508@gmail.com>
2025-07-26 07:42:33 +0000

    feat : order api v3.1

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -11,16 +11,13 @@ import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Root;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
-import jpabook.jpashop.domain.OrderStatus;
-import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Predicate;
-import jpabook.jpashop.domain.Address;
+
 import jpabook.jpashop.domain.Member;
 
-import java.time.LocalDateTime;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -104,12 +101,29 @@ public class OrderRepository {
             .setParameter("name", "kim")
             .getResultList();
     return  orders;
-}
+    }
     public List<Order> findAllWithMemberDelivery(){
         return em.createQuery(
             "select o from Order o" +
             " join fetch o.member m" +
             " join fetch o.delivery d", Order.class
         ).getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit){
+        return em.createQuery(
+            "select o from Order o" +
+            " join fetch o.member m" +
+            " join fetch o.delivery d", Order.class
+        ).setFirstResult(offset).setMaxResults(limit)
+        .getResultList();
+    }
+
+    public List<Order> findAllWithItem(){
+        return em.createQuery("select distinct o from Order o" + 
+            " join fetch o.member m" +
+            " join fetch o.delivery d" +
+            " join fetch o.orderItems oi" +
+            " join fetch oi.item i", Order.class).getResultList();  
     }
 }
